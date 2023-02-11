1e6fd0bb685fc7c95fb02f45d81febd871f83e78
Good Guy <123004164+good-guy1218@users.noreply.github.com>
2023-02-11 08:02:38 -0800

    remove api information

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 
 import { Platform,Dimensions} from 'react-native';
-//export const API_URL = Platform.OS === 'ios' ? 'https://pikselpartybackend.herokuapp.com/users' : 'http://192.168.111.214:4000/users';
-export const API_URL = Platform.OS === 'ios' ? 'https://pikselpartybackend.herokuapp.com/users' : 'https://pikselpartybackend.herokuapp.com/users';
+//export const API_URL = Platform.OS === 'ios' ? 'API_URL' : 'API_URL';
+export const API_URL = Platform.OS === 'ios' ? API_URL : API_URL;
 //export const SOCKET_URL = 'http://192.168.111.214:3000';
- export const SOCKET_URL = 'https://pikselpartysocketserver.herokuapp.com';
+ export const SOCKET_URL = SOCKET_URL;
 
-export const STORAGE_KEY = "@PikselParty:2021";
+export const STORAGE_KEY = "@STORAGE_KEY";
 
 export const default_photo = 'https://res.cloudinary.com/dpyy2o3xv/image/upload/v1629305856/selfie_zben0m.png';
 export const upload_url = 'https://api.cloudinary.com/v1_1/dpyy2o3xv/image/upload';
